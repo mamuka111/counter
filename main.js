cd708f3d2fff5d7caf85e9fc0f3348960b49b9a8
mamuka111 <123620275+mamuka111@users.noreply.github.com>
2023-02-24 20:42:08 +0400

    finished JS part

--- main.js
+++ main.js
@@ -1,3 +1,22 @@
-let dicrease = document.querySelector(".dicrease")
+let decrease = document.querySelector(".decrease")
 let reset = document.querySelector(".reset")
 let increase = document.querySelector(".increase")
+let main = document.querySelector(".main")
+let p = 0;
+main.innerHTML = p;
+decrease.addEventListener("click", function() {
+    p--;
+    main.innerHTML = p;
+    main.style.color = 'red';
+  });
+  reset.addEventListener("click", function() {
+    p = 0;
+    main.innerHTML = p;
+    main.style.color = 'black';
+
+  });
+  increase.addEventListener("click", function() {
+    p++;
+    main.innerHTML = p;
+    main.style.color = 'green';
+  });
